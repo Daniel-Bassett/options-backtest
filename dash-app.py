a1526b25a96d2781769b376bbf154fe42f31f625
Daniel B <daniel.m.bassett@outlook.com>
2023-01-22 16:23:55 -0600

    now dates show in hovertext labels

--- dash-app.py
+++ dash-app.py
@@ -33,34 +33,13 @@ import pickle
 meta_tags = [{'name': 'viewport', 'content': 'width=device-width'}]
 external_stylesheets = [meta_tags]
 
-app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+app = dash.Dash(__name__, external_stylesheets=[dbc.themes.MINTY])
 server = app.server
-
-ticker = 'AAPL'
-the_date = 'agg'
-expiration = 'near'
-agg = pickle.load(open(f'data/{ticker}/{ticker}-{the_date}-{expiration}.pickle','rb'))
-avg_days = agg['avg_days']
-
-fig = px.imshow(agg['mean'].round(1), color_continuous_scale=[(0,'red'), (0.5,'white'), (1.0, 'green')], range_color=(-100 ,100), text_auto=True)
-fig.update(data=[{'customdata': np.dstack((agg['median'], agg['max'], agg['min'])),
-    'hovertemplate': '<b>mean:%{z:.1f}</b> <br>median: %{customdata[0]:.1f} <br>max: %{customdata[1]:.1f} <br>min: %{customdata[2]:.1f}<extra></extra>'}])
-fig.update_xaxes(rangebreaks=[dict(bounds=["sat", "mon"])]),  # hide weekends, eg. hide sat to before mon])
-fig.update_yaxes(autorange='reversed')
-fig.update_layout(
-    title=f'{ticker} ATM Straddle Performance in percent<br>expires ~{avg_days} days after earnings',
-    title_x=0.5,
-    yaxis_title='Straddle Initiated',
-    xaxis_title='Trading Days Remaining',
-    width=1000,
-    height=1000
-    )
-
 
 
 app.layout = html.Div([
 
-        html.Div([
+        dbc.Row([
             html.Div([
                 html.H5('Straddle Performance', className='title_text')
             ], className='title_container twelve columns'),
@@ -69,15 +48,23 @@ app.layout = html.Div([
 
 
         
-        html.Div([
+        dbc.Row([
+            dbc.Col([
+                dcc.Dropdown(options=['AAPL', 'AMZN', 'FDX', 'NVDA', 'BAC'], value='AAPL', id='ticker'),
+                dcc.Dropdown(id='earnings_dates', value='agg')
+                ], width=2, class_name='flex_display'),
+            dbc.Col([
+                dcc.RadioItems(options=['near', 'med', 'far'], value='near', id='horizon')
+                ], width=2, class_name='flex_display'),
+        ]),
+
             
-            dbc.Col([ 
-                dcc.Dropdown(options=['AAPL', 'NVDA', 'AMZN', 'FDX'], value='AAPL', id='ticker'),
-                dcc.Dropdown(id='earnings_dates', value='agg'),
-                dcc.RadioItems(options=['near', 'med', 'far'], value='near', id='horizon'),
+        dbc.Col([ 
+            dbc.Row([
                 dcc.Graph(id='graph-output')
-            ], className='graph'),
-        ])
+            ],className='graph')
+        ], width=9)
+        
         
         
 
@@ -93,16 +80,16 @@ app.layout = html.Div([
 @app.callback(Output(component_id='earnings_dates', component_property='options'),
              [Input(component_id='ticker', component_property='value')])
 def get_earnings_dates(ticker):
-    stock = yf.Ticker(ticker)
-    earnings_dates = stock.get_earnings_dates(limit=11).dropna(axis=0).reset_index()
-
-    # if earnings is after market close, set earnings date to next day
-    earnings_dates.loc[earnings_dates['Earnings Date'].dt.hour > 6, 'Earnings Date'] =  earnings_dates['Earnings Date'] + dt.timedelta(days=1)
-
-    earnings_dates = earnings_dates['Earnings Date'].dt.date
-    earnings_dates = [str(date) for date in earnings_dates]
-    earnings_dates.append('agg')
+    earnings_dates = pickle.load(open(f'data/{ticker}/{ticker}-earnings-dates.pickle','rb'))
+    earnings_dates.insert(0, 'agg')
     return [{'label': i, 'value': i} for i in earnings_dates]
+
+
+@app.callback(Output(component_id='earnings_dates', component_property='value'),
+             [Input(component_id='ticker', component_property='value')])
+def get_earnings_dates(ticker):
+     return 'agg'
+
 
 # make graph
 @app.callback(Output(component_id='graph-output', component_property='figure'),
@@ -129,9 +116,37 @@ def display_data(ticker, earnings_date, horizon):
             )
         return fig
     else:
-        days_df = pickle.load(open(f'data/{ticker}/{ticker}-{earnings_date}-{horizon}.pickle','rb'))
-        days_after_earnings = days_df['days after earnings']
-        fig = px.imshow(days_df['df'].round(1), color_continuous_scale=[(0,'red'), (0.5,'white'), (1.0, 'green')], range_color=(-100 ,100), text_auto=True)
+        try:
+            df = pickle.load(open(f'data/{ticker}/{ticker}-{earnings_date}-{horizon}.pickle','rb'))
+            days_df = df['days_df']
+            date_df = df['date_df']
+            days_after_earnings = df['days after earnings']
+            fig = px.imshow(days_df.round(1), color_continuous_scale=[(0,'red'), (0.5,'white'), (1.0, 'green')], range_color=(-100 ,100), text_auto=True)
+            fig.update(data=[{'customdata': np.dstack((days_df.round(1), date_df)),
+                'hovertemplate': '<b>return: %{z:.1f}</b> <br>date: %{customdata[1]}<extra></extra>'}])
+            fig.update_layout(
+                title=f'{ticker} ATM Straddle Performance in percent (expires {days_after_earnings} days after earnings) for {earnings_date}',
+                title_x=0.5,
+                yaxis_title='Straddle Initiated',
+                xaxis_title='Trading Days Remaining',
+                width=1000,
+                height=1000
+            )
+            fig.update_xaxes(rangebreaks=[dict(bounds=["sat", "mon"])]),  # hide weekends, eg. hide sat to before mon])
+            return fig
+        except:
+            fig = px.scatter_3d(title="Select a Valid Date")
+            return fig
+
+                
+
+    
+        days_df = df['days_df']
+        date_df = df['date_df']
+        days_after_earnings = df['days after earnings']
+        fig = px.imshow(days_df.round(1), color_continuous_scale=[(0,'red'), (0.5,'white'), (1.0, 'green')], range_color=(-100 ,100), text_auto=True)
+        fig.update(data=[{'customdata': np.dstack((days_df.round(1), date_df)),
+            'hovertemplate': '<b>return: %{z:.1f}</b> <br>date: %{customdata[1]}<extra></extra>'}])
         fig.update_layout(
             title=f'{ticker} ATM Straddle Performance in percent (expires {days_after_earnings} days after earnings) for {earnings_date}',
             title_x=0.5,
